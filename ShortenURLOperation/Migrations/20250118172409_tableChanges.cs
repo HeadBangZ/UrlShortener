6b2484b02000b6d610224dd031d0e770d49f9852
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2025-01-18 18:24:50 +0100

    Table changes, made code as an index and unique, and max length 7

---
+++ ShortenURLOperation/Migrations/20250118172409_tableChanges.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShortenURLOperation.Migrations
+{
+    /// <inheritdoc />
+    public partial class tableChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ShortenedUrls",
+                type: "character varying(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortenedUrls_Code",
+                table: "ShortenedUrls",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortenedUrls_Code",
+                table: "ShortenedUrls");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ShortenedUrls",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(7)",
+                oldMaxLength: 7);
+        }
+    }
+}
