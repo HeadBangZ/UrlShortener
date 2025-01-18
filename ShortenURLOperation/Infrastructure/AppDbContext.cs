df45d3516142339add2abab871aab93fc6f1af98
Thomas Nordvig Hermansen <thomasnhprivat@gmail.com>
2025-01-18 18:25:13 +0100

    Table changes, made code as an index and unique, and max length 7 (#2)
    Co-authored-by: Thomas Nordvig Hermansen <nordvigprivat@gmail.com>

--- ShortenURLOperation/Infrastructure/AppDbContext.cs
+++ ShortenURLOperation/Infrastructure/AppDbContext.cs
@@ -8,4 +8,17 @@ public class AppDbContext : DbContext
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
 	public DbSet<ShortenedUrl> ShortenedUrls => Set<ShortenedUrl>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<ShortenedUrl>()
+			.HasIndex(x => new { x.Code })
+			.IsUnique();
+
+		modelBuilder.Entity<ShortenedUrl>()
+			.Property(x => x.Code)
+			.HasMaxLength(7);
+	}
 }
--- ShortenURLOperation/Migrations/20250118172409_tableChanges.Designer.cs
+++ ShortenURLOperation/Migrations/20250118172409_tableChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShortenURLOperation.Infrastructure;
@@ -11,9 +12,11 @@ using ShortenURLOperation.Infrastructure;
 namespace ShortenURLOperation.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250118172409_tableChanges")]
+    partial class tableChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
